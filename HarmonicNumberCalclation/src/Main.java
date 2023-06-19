a50837660076774d46052b6750585611963e798d
Büşra Örnek <107885877+busraornek@users.noreply.github.com>
2023-06-19 12:47:12 +0300

    modified mian.java

--- HarmonicNumberCalclation/src/Main.java
+++ HarmonicNumberCalclation/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the N value:");
+        int n = input.nextInt();
+        double result = 0;
 
-
+        for(double i = 1; i <= n; i++){
+            result += (1.0 / i);
+        }
+         System.out.println(result);
     }
 }
